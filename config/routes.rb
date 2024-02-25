fab8ecc62306fac1044536e85d1083ab2096f527
Lucas Moreno <lucaslopesmoreno@gmail.com>
2024-02-24 22:47:18 -0300

    restaurant with reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'reviews/new'
+  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # get "up" => "rails/health#show", as: :rails_health_check
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: "new_restaurant_review"
+  # post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: "restaurant_reviews"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "2345-1234", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "3245-3457", category: "italian"}
+brazilbowl = {name: "BrazilBowl", address: "24 Paulista St, Sao Paulo", phone_number: "6332-5677", category: "japanese"}
+koreanbbq = {name: "KoreanBBQ", address: "12 Korean Av, Korea", phone_number: "4566-8765", category: "french"}
+burgerhouse = {name: "BurgerHouse", address: "99 Sorocaba, Brazil", phone_number: "7564-8457", category: "belgian"}
+
+[dishoom, pizza_east, brazilbowl, koreanbbq, burgerhouse].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
